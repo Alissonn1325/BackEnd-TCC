9332e0f8a40d5a14c294be062ac962b1398b93c7
Alissonn1325 <alissonfbrito01@gmail.com>
2024-08-20 15:51:33 -0300

    Inclusão do Def (Tinha Esquecido)

--- core/models/especie.py
+++ core/models/especie.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Especie(models.Model):
     especie = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"ID: ({self.id}) |:| Espécie: {self.especie} |:|"
--- core/models/raca.py
+++ core/models/raca.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Raca(models.Model):
-    Raca = models.CharField(max_length=100)
+    raca = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"ID: ({self.id}) |:| Raça: {self.raca} |:|"
