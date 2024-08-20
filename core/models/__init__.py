f7d40a9a9e8b5120fedf29c283b3d81c63a56cc1
Alissonn1325 <alissonfbrito01@gmail.com>
2024-08-20 14:38:18 -0300

    Criação da Model Especie

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,2 +1,3 @@
 from .user import User
 from .situacao import Situacao
+from .especie import Especie
---
+++ core/models/especie.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Especie(models.Model):
+    especie = models.CharField(max_length=100)
+    
+
+    
