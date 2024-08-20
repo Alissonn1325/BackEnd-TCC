46abee16226f83005902bfef575fae1fd0ec0156
Alissonn1325 <alissonfbrito01@gmail.com>
2024-08-20 16:00:43 -0300

    Criação da Model, Inserção no Admin e Migração Adoção

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User
 from .situacao import Situacao
 from .especie import Especie
 from .raca import Raca
+from .adocao import Adocao
---
+++ core/models/adocao.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+
+class Adocao(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="adocoes")
+    data_adocao = models.DateField()
+    observacoes = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return f'Adoção #{self.id} por {self.user}'
